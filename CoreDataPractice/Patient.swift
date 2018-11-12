7a4cda224bed813d7b5797936a780cf13ce91e4a
Ahmed Eltabbal <atgeddan@gmail.com>
2018-11-12 21:10:41 +0200

    Initial Commit

---
+++ CoreDataPractice/Patient.swift
@@ -0,0 +1,33 @@
+//
+//  Patient.swift
+//  CoreDataPractice
+//
+//  Created by Ahmed Eltabbal on 11/11/18.
+//  Copyright © 2018 Ahmed Eltabbal. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Patient: NSManagedObject {
+  
+  
+  func savePatientWithInfo(info:[String:String] ,context: NSManagedObjectContext) {
+    if let name2 = info["name"] {
+      name = name2
+    }
+    if let age2 = info["age"] {
+      age = age2
+    }
+    if let gender2 = info["gender"] {
+      gender = gender2
+    }
+    do {
+      try context.save()
+      print("Saved the Patient Successfuly")
+    } catch {
+      print(error)
+    }
+  }
+  
+}
---
+++ CoreDataPractice/homeCell/homeCell.swift
@@ -0,0 +1,24 @@
+//
+//  homeCell.swift
+//  CoreDataPractice
+//
+//  Created by Ahmed Eltabbal on 11/12/18.
+//  Copyright © 2018 Ahmed Eltabbal. All rights reserved.
+//
+
+import UIKit
+
+class homeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
